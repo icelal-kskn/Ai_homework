b8532f91e30e82d916077939db1c3a924795ed3c
Celal İkram KESKİN <72507616+icelal-kskn@users.noreply.github.com>
2024-01-02 13:04:42 +0300

    pdf and docx uploaded and pa3.py file changed

--- PA3/AI_PA3.py
+++ PA3/AI_PA3.py
@@ -1,119 +1,164 @@
+# Mert Kardaş 200316045
+# İkram Celal KESKİN 200316059
+# Musa Sina ERTUĞRUL 200316011
 import random
-from simpleai.search import SearchProblem, astar, breadth_first, depth_first
+from simpleai.search import SearchProblem, astar, breadth_first, depth_first, genetic, hill_climbing, greedy
 from simpleai.search.viewers import BaseViewer
+from time import perf_counter
 
-
-#class definition for NQueens
 class NQueens(SearchProblem):
-
-    def __init__(self, N):
-        self.N=N
-        self.state=self._set_state()
-        super().__init__(initial_state=self.state)
+    def __init__(self,*, N:int =None, state=None):
+        
+        self.state = list()
+        if state is None and isinstance(N, int): 
+            if N > 0: 
+                self.n = N
+            else: 
+                raise ValueError('N must be greater than 0 at least')
+            print('How would you like to set the state?\n',
+                'Type 1 for manually\n',
+                'Type 2 for random size')
+            input_ = input('Enter no:')
+            if input_ == str(1):
+                self._set_state()
+            elif input_ == str(2):
+                    self.state = self.generate_random_state()
+        elif state is not None and len(state) == N or N is None: 
+            self.n = len(state)
+            self.state = state 
+        
+        else: 
+            raise ValueError("Invalid arguments")
+        
+            
+        super().__init__(initial_state=tuple(self.state))
 
     def __str__(self):
-        return f"State: {self.state}"
-      
+        return f'N = {self.n}, state = {self.state}'
+   
     def _set_state(self):
-        return self.generate_random_state()
-    
-    #TODO  SetState is disabled please close the test and open the following codes using Ctrl+K+C or Ctrl+Ö 
-        
-        # choice = input("Manually enter state (M) or generate a random state (R)? ").strip().lower()
-        # if choice.upper() == 'M':
-        #     state = input(f"Enter a state of length {self.N}: ").strip()
-        #     if self._is_valid(state):
-        #         return state
-        #     else:
-        #         print("Invalid state. Please try again.")
-        #         return self._set_state()
-        # elif choice.upper() == 'R':
-        #     return self.generate_random_state()
-        # else:
-        #     print("Invalid choice. Please enter 'M' or 'R'.")
-        #     return self._set_state()   
-        
-    def generate_random_state(self):
-        while True:
-            state = ''.join(str(random.randint(1, self.N)) for _ in range(self.N))
-            if self._is_valid(state):
-                return state  
+        manual = input('put the queens: ')
+        if self._is_valid(manual):
+            for i in manual:
+                self.state.append(int(i))
+        else:
+            print('Invalid state!!!')
 
-    def _is_valid(self,state):
-        #Case 1
-        if not state.isdigit():
-            return False
-        #Case 2
-        if len(state) != self.N:
-            return False
-        #Case 3
-        if any(int(row) < 0 or int(row) > self.N for row in state):
-            return False 
-        return True  
-  
-    def _count_attacking_pairs(self, state):
-        count = 0
-        for i in range(self.N):
-            for j in range(i + 1, self.N):
-                if state[i] == state[j] or abs(int(state[i]) - int(state[j])) == abs(i - j):
-                    count += 1
-        return count
+    def generate_random_state(self)-> list:
     
+        return [random.randint(1, self.n) for _ in range(self.n)]
+
+    def _is_valid(self, state) -> bool:
+        try:
+            int(state)
+        except ValueError:
+            return False
+        if len(state) != self.n:
+            return False
+        for i in state:
+            if int(i) > self.n:
+                return False
+            if int(i) <= 0:
+                return False
+        return True
+
+    def _count_attacking_pairs(self, state: list):
+        att_pairs = 0
+        for i in range(0, self.n - 1):
+            for j in range(i + 1, self.n):
+                if state[i] == state[j] or i - j == state[i] - state[j] or abs(i - j) == state[i] - state[j] :
+                    att_pairs += 1
+        return att_pairs
+
     def actions(self, state):
-        # Returns a list of possible actions from the current state
         possible_actions = []
-        for i in range(self.N):
-            for j in range(1, self.N + 1):
-                if str(j) != state[i]:
-                    possible_actions.append((i, str(j)))
+        for i in range(self.n):
+            for j in range(1, self.n + 1):
+                if state[i] != j:
+                    possible_actions.append(f'Queen collum {i + 1}, row {state[i]} is moved to row {j} ')
         return possible_actions
-
-    def result(self, state, action):
-        column, new_row = action
-        new_state = list(state)
-        new_state[column] = new_row
-        return ''.join(new_state)
-
-    def is_goal(self, state):
-        # Returns whether the state given as a parameter is a goal state
-        return self._count_attacking_pairs(state) == 0
-
-    def heuristic(self, state):
-        # Returns the estimated solution cost from the given state to the goal state
-        return self._count_attacking_pairs(state)
-
-    def cost(self, state1, action, state2):
+    
+    def cost(self, state, action, state2):
         return 1
+    
+    def result(self, state, action):
+        new_state = list(state)
+        digits =[i for i in action if i.isdigit()]
+        column_index = int(digits[0]) 
+        new_row = int(digits[2]) 
+        new_state[column_index -1] = new_row
+        return tuple(new_state)
+
+    
+    def is_goal(self, state):
+        if self._count_attacking_pairs(list(state)) == 0:
+            print('Goal Node')
+            return True
+        
+    def heuristic(self, state):
+        return self._count_attacking_pairs(list(state))
+    
+    def value(self, state):
+        return  -(self._count_attacking_pairs(state))
+
+    def crossover(self, state1, state2):
+        cut_point = random.randint(0,self.n -1)
+        child = state1[:cut_point] + state2[cut_point:]
+        return child
+    
+    def mutate(self, state):
+        state = list(state)
+        mut_point = random.randint(0,self.n -1)
+        state[mut_point] = random.randint(1,self.n)
+        return state 
+         
 
 
+ 
+class Testing: 
+    def __init__(self, problem, iteration_limit=0): 
+        self.problem= problem
+        self.functions = [breadth_first, depth_first, astar, genetic, hill_climbing, greedy]
+        self.viewer =BaseViewer()
+        self.iteration_limit = iteration_limit
+    
+        
+    def test(self): 
+        for problem in self.problem:
+            for function in self.functions:
+                self._tester(problem, function)
+                    
+    def _tester(self, problem, func):
+        if  func == hill_climbing: 
+            start = perf_counter()
+            test = func(problem, self.iteration_limit, self.viewer)
+            finish = perf_counter()
+        elif func ==genetic: 
+            start = perf_counter()
+            test = func(problem)
+            finish = perf_counter()
+        else:
+            start = perf_counter()
+            test = func(problem, graph_search=True, viewer=self.viewer)
+            finish = perf_counter()
+        
+        print(f'''
+            Function: {func}
+            Initial state: {problem.state}
+            Fİnal State: {test.state}
+            Path: {test.path()}
+            Cost: {test.cost}
+            Stats: {self.viewer.stats}
+            Execution Time: {(finish - start):.3f}s
+        ''')
+        print('-----------------------------------------------')
+problems=[
+    NQueens(state=[2,3,2,3]),
+    NQueens(state=[4,3,1,1]),
+    NQueens(state=[3,4,4,2]),
+    NQueens(state=[1,2,3,4,5]),
+    NQueens(state=[1,3,1,5,4])
+]
 
-def test_algorithm(algorithm, N, initial_state):
-    problem = NQueens(N)
-    viewer = BaseViewer()  
-
-    print(f"\nTesting {algorithm} with N={N} and initial state: {initial_state}")
-    if algorithm == astar or algorithm == breadth_first or algorithm == depth_first:
-        result = algorithm(problem, viewer=viewer)
-        print("Algorithm:", algorithm)
-        print("Resulting State:", result.state)
-        print("Resulting Path:", result.path())
-        print("Cost of Solution:", result.cost)
-        print("Viewer Statistics:", viewer.stats)
-    else:
-        print("Invalid algorithm choice.")
-
-
-initial_states = ["2323", "4311", "3442", "12345", "13154", "536142", "532512"]
-
-# Test each algorithm for initial states
-for initial_state in initial_states:
-    N = len(initial_state)
-    test_algorithm(astar, N, initial_state)
-
-for initial_state in initial_states:
-    N = len(initial_state)
-    test_algorithm(breadth_first, N, initial_state)
-
-for initial_state in initial_states:
-    N = len(initial_state)
-    test_algorithm(depth_first, N, initial_state)
+test = Testing(problems, iteration_limit=10)
+test.test()
